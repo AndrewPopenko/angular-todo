332f6366494fce30fda098a47e77a9bbe9b3e6c2
AndrewPopenko <andrew.popenko@gmail.com>
2021-02-15 01:09:23 +0100

    rxjs suppotr added

--- src/app/service/data-handler.service.ts
+++ src/app/service/data-handler.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Subject } from 'rxjs';
 import { TestData } from '../data/TestData';
 import { Category } from '../model/Category';
 import { Task } from '../model/Task';
@@ -8,13 +9,30 @@ import { Task } from '../model/Task';
 })
 export class DataHandlerService {
 
+  taskSubject = new Subject<Task[]>();
+
   constructor() { }
 
   getCategories(): Category[] {
     return TestData.categories;
   }
 
+  fillTasks() {
+    this.taskSubject.next( TestData.tasks );
+  }
+
   getTasks(): Task[] {
     return TestData.tasks;
+  }
+
+  fillTasksByCategory(category: Category) {  
+    const tasks = TestData.tasks.filter( item => item.category === category);
+    this.taskSubject.next( tasks );
+  }
+
+  getTasksByCategory(category: Category): Category[] {
+    const tasks = TestData.tasks.filter( item => item.category === category);
+    console.log(tasks);
+    return tasks;
   }
 }
