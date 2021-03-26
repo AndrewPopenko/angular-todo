1bb046a84be694538927fac9590bbd1a16578705
AndrewPopenko <andrew.popenko@gmail.com>
2021-03-26 23:58:29 +0100

    support mat-table

--- src/app/views/tasks/tasks/tasks.component.ts
+++ src/app/views/tasks/tasks/tasks.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 import { Task } from 'src/app/model/Task';
 import { DataHandlerService } from 'src/app/service/data-handler.service';
 
@@ -11,16 +12,33 @@ import { DataHandlerService } from 'src/app/service/data-handler.service';
 export class TasksComponent implements OnInit {
 
   tasks: Task[];
+  dataSource: MatTableDataSource<Task>;
+  displayedColumns: string[] = ['color', 'id', 'title', 'date', 'priority', 'category'];
 
   constructor(private dataHandler: DataHandlerService) {
     this.tasks = [];
+    this.dataSource = new MatTableDataSource();
    }
 
   ngOnInit(): void {
     this.dataHandler.taskSubject.subscribe( tasks => this.tasks = tasks );
+    // this.dataSource = new MatTableDataSource();
+    this.refreshTable();
   }
 
   toggleTaskCompleted(task: Task) {
     task.completed = !task.completed;
+  }
+
+  getPriorityColor(task: Task): string {
+    if (task.priority && task.priority.color) {
+      return task.priority.color;
+    }
+
+    return '#fff';
+  }
+
+  private refreshTable() {
+    this.dataSource.data = this.tasks;
   }
 }
